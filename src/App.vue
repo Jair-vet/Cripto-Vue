99551a5afc32501ac70740257c6df98df915dadf
Jair <jairaceves56@gmail.com>
2023-11-23 15:15:04 -0600

    Hacemos el fetch a la Api

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script setup>
+  import { ref, onMounted } from 'vue';
+
+
+  const monedas = ref([
+      { codigo: 'USD', texto: 'Dolar de Estados Unidos'},
+      { codigo: 'MXN', texto: 'Peso Mexicano'},
+      { codigo: 'EUR', texto: 'Euro'},
+      { codigo: 'GBP', texto: 'Libra Esterlina'},
+  ])
+
+  onMounted(() => {
+    fetch('https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD')
+      .then(respuesta => respuesta.json())
+      .then(data => {
+        console.log(data);
+      })
+  })
+
+</script>
+
+<template>
+  <div class="contenedor">
+    <h1 class="titulo">Cotizador de <span>Criptomonedas</span></h1>
+
+    <div class="contenido">
+      <form class="formulario">
+        <div class="campo">
+          <label for="moneda">Moneda:</label>
+          <select id="moneda">
+            <option value="">- - Selecciona - -</option>
+            <option 
+              v-for="moneda in monedas" 
+              v-bind:key="moneda.codigo"
+              :value="moneda.codigo"
+            >{{ moneda.texto }}</option>
+
+          </select>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
