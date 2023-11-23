7eea5338a99c78db8a05f7ba3805f7d6967aec22
Jair <jairaceves56@gmail.com>
2023-11-23 17:07:24 -0600

    Agregamos color a la Alerta

--- src/components/Alerta.vue
+++ src/components/Alerta.vue
@@ -24,10 +24,11 @@
         text-align: center;
         margin-bottom: 2rem;
         color: var(--blanco);
-        background-color: rgb(174, 0, 0);
+        background-color: rgb(196, 23, 23);
         text-transform: uppercase;
         font-weight: 700;
         font-size: 1.4rem;
-        padding: .5rem
+        padding: 1rem;
+        border-radius: 5px;
     }
 </style> 
